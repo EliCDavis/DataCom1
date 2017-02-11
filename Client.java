be3aaeb0b0c1a1ec853bdefa2dae7cca7eafa040
Eli Davis <eli.davis1995@gmail.com>
2017-02-10 22:43:55 -0600

    Finished Assignment

--- Client.java
+++ Client.java
@@ -1,40 +1,43 @@
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.nio.charset.Charset;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * A client that attempts to upload a file to a specified server
+ * 
+ * References:
+ * http://stackoverflow.com/questions/23276407/how-to-read-from-files-with-files-lines-foreach
  *
  * @author Eli
  */
-public class Client {
+public class client {
 
     public static void main(String[] args) {
 
         // Formatting
         System.out.println("");
 
+        // Clean Arguements
         if (args.length != 3) {
             System.out.println("Error: Expecting 3 arguments");
             System.out.println("    Example:  localhost 6003 file.txt");
             return;
         }
 
+        // Negotiation port
         int port;
 
+        // Try parsing port from arguments
         try {
             port = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
@@ -43,121 +46,174 @@ public class Client {
             return;
         }
 
+        // Grab server name and file name from rest of arguments
         String server = args[0];
         String fileToCopy = args[2];
 
+        // Connects to negotiation server port and get's a new port number for file transfer
         int transactionPort = GetTransactionPort(server, port);
 
+        // If we where unable to grab the file transfer port, display error
         if (transactionPort == -1) {
             System.err.println("Error grabbing transaction port #. Client shutting down");
             return;
         }
 
-        String errorMessage = SendFile(server, transactionPort, fileToCopy);
-
-        if (errorMessage == null) {
-            System.out.println("File succesfully uploaded to server");
-        } else {
-            System.err.println("Transfer Error: \n\t" + errorMessage);
+        // Attempt to send the file specified in arguments to the port we
+        // recieved from the negotiation process
+        try {
+            SendFile(server, transactionPort, fileToCopy);
+        } catch (IOException ex) {
+           System.err.println("Transfer Error: \n\t" + ex.getMessage());
+           System.out.println("Transer terminated early");
         }
 
     }
 
+    /**
+     * Creates a TCP socket and attempts to connect to a server given
+     * name and port for negotiating a port number for performing file transfer
+     * 
+     * @param server name such as 'localhost'
+     * @param port the server is running on
+     * @return port for file transfer
+     */
     public static int GetTransactionPort(String server, int port) {
         try {
 
             Socket connection = new Socket(server, port);
-
+            
             // Get the stream to write to our socket
             OutputStream output = connection.getOutputStream();
-
+            
             // in stream
             Scanner s = new Scanner(connection.getInputStream());
-
+            
             // Send the port we want to use for transaction
-            output.write("123\n".getBytes(Charset.forName("UTF-8")));
+            output.write("123\n".getBytes("UTF-8"));
 
-            System.out.println("Sent 123");
-
+            // Read an integer from what was pushed over the wire
             int transactionPort = s.hasNext() ? s.nextInt() : -1;
 
-            System.out.println("Recieved over socket: " + transactionPort);
+            // Display port we recieved
+            System.out.println("Random port: " + transactionPort + "\n");
 
+            // clean up connections
             connection.close();
 
             return transactionPort;
 
         } catch (IOException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
             return -1;
         }
 
     }
 
-    public static String SendFile(String server, int port, String filename) {
+    /**
+     * Attempts to connect to the specified server name and port and send a
+     * file encoded as ASCII.
+     * 
+     * @param server name for transfer
+     * @param port for transfer
+     * @param filename name of file to transfer
+     * @return An error message if one occured
+     * @throws IOException 
+     */
+    public static String SendFile(String server, int port, String filename) throws IOException {
 
         DatagramSocket socket;
         DatagramPacket inPacket = new DatagramPacket(new byte[4], 4);
         DatagramPacket outPacket = new DatagramPacket(new byte[4], 4);
         InetAddress address;
-        
+        BufferedReader br;
+        FileReader fr;
+
         // Get IP server
         try {
             address = InetAddress.getByName(server);
         } catch (UnknownHostException ex) {
             return ex.getMessage();
         }
-        
+
         // Setup packet route
         outPacket.setAddress(address);
         outPacket.setPort(port);
-        
+
         // Initialize client socket
-        try {
-            socket = new DatagramSocket();
-        } catch (SocketException ex) {
-            return ex.getMessage();
+        socket = new DatagramSocket();
+
+        // Open the file to send
+        fr = new FileReader(client.class.getResource(filename).getPath());
+
+        br = new BufferedReader(fr);
+
+        int character;
+        String currentPayload = "";
+
+        // Continue reading until we get through file
+        while (true) {
+            
+            // try reading a character in from the buffer.
+            character = br.read();
+
+            // If -1 we're end of file, else continue building payload
+            if (character == -1) {
+                break;
+            } else {
+                currentPayload += (char)character;
+            }
+            
+            // Talk to peer if we've reached 4 characters
+            if (currentPayload.length() == 4) {
+                // Send data
+                System.out.println(pushSocketData(socket, inPacket, outPacket, currentPayload));
+                currentPayload = "";
+                
+            }
+
         }
-        
-        for (int i = 0; i < 32; i ++) {
-            
-            // Create packet data
-            try {
-                outPacket.setData( ( i == 31? "BBB\4":i+"aa"+i).getBytes("US-ASCII") );
-            } catch (UnsupportedEncodingException ex) {
-                return ex.getMessage();
-            }
-            
-            // Send packet data
-            try {
-                socket.send(outPacket);
-            } catch (IOException ex) {
-                return ex.getMessage();
-            }
-            
-            // Grab a packet of data
-            try {
-                socket.receive(inPacket);
-            } catch (IOException ex) {
-                return ex.getMessage();
-            }
-            
-            String payload;
-            
-            // Grab payload
-            try {
-                payload = new String(inPacket.getData(), "US-ASCII");
-            } catch (UnsupportedEncodingException ex) {
-                return ex.getMessage();
-            }
-            
-            System.out.print(payload);
-            
-        }
-        
-        
+
+        // Finnish up our last payload, adding \4 (End of Transmission) to 
+        // signalfy we're done transfering
+        currentPayload += "\4";
+        System.out.println(pushSocketData(socket, inPacket, outPacket, currentPayload));
+
+        socket.close();
+
         return null;
+
+    }
+    
+    /**
+     * Pushes data to the socket that's been opened up.
+     * 
+     * @param socket the socket we want to communicate with
+     * @param inPacket the response will be stored in this packet
+     * @param outPacket our message will be stored in this packet
+     * @param payload The data we want to send over the socket
+     * @return the response message from the socket
+     * @throws IOException When the ack from the server is incorrect
+     */
+    private static String pushSocketData(DatagramSocket socket, DatagramPacket inPacket, DatagramPacket outPacket, String payload) throws IOException{
         
+        // Send a packet
+        outPacket.setData(payload.getBytes("US-ASCII"));
+        socket.send(outPacket);
+
+        // Get response
+        socket.receive(inPacket);
+        String response =  new String(inPacket.getData(), "US-ASCII");
+        
+        
+        // Throw an error if the ack is illegal
+        if(!response.equalsIgnoreCase(payload)){
+            throw new IOException("Malformed ack from server");
+        }
+        
+        // return correct response from server
+        return response;
     }
 
+    
 }
--- Server.java
+++ Server.java
@@ -1,41 +1,41 @@
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.net.SocketException;
-import java.nio.charset.Charset;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
+ * A simple socketed server implementation that uses TCP for port negotiation
+ * that is then used for UDP file transfer.  Files are written to output.txt
+ * 
  * References: https://systembash.com/a-simple-java-tcp-server-and-tcp-client/
  * http://stackoverflow.com/questions/4069028/write-string-to-output-stream
  * http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
  *
  * @author Eli
  */
-public class Server {
-    
-    static int transactionServerPort = 3000;
-    
+public class server {
+
     public static void main(String[] args) {
 
-        // Formatting
-        System.out.println("");
-        
+        // Make sure port is passed in
         if (args.length != 1) {
             System.out.println("Error: Expecting port # as only arguemnet");
             System.out.println("    Example:  java Server 2000");
             return;
         }
-        
+
+        // Port for negotiation
         int port;
-        
+
+        // Try parsing integer out of arguemnt
         try {
             port = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
@@ -43,92 +43,102 @@ public class Server {
             System.out.println("    Example:  java Server 2000");
             return;
         }
+
+        // A socket for file transfer
+        DatagramSocket socketForTransfer = null;
         
-        String negotiationErrorMessage = StartNegotiationServer(port);
-        
-        if (negotiationErrorMessage != null && negotiationErrorMessage.length() != 0) {
-            System.err.println("Negotiation Error: \n\t" + negotiationErrorMessage);
-            return;
+        // Listen Negotiation
+        try {
+             socketForTransfer = StartNegotiationServer(port);
+        } catch (IOException ex) {
+            System.err.println("Negotiation Error: \n\t" + ex.getMessage());
         }
-        
-        String transferErrorMessage = StartTransferServer(transactionServerPort);
-        
+
+        // Start server for file transfer and catch errors
+        String transferErrorMessage = StartTransferServer(socketForTransfer);
+
+        // Print out error message if one exists
         if (transferErrorMessage != null && transferErrorMessage.length() != 0) {
             System.err.println("Transfer Error: \n\t" + transferErrorMessage);
-            return;
         }
-        
-        System.out.println("Assignment Complete!");
-        
+
     }
-    
-    public static String StartNegotiationServer(int port) {
-        
+
+    /**
+     * Starts a TCP socket server for clients to connect to.  Returns a UDP socket
+     * that clients will later talk with.
+     * 
+     * @param port number for negotiation
+     * @return a socket for file transfer
+     * @throws IOException 
+     */
+    public static DatagramSocket StartNegotiationServer(int port) throws IOException {
+
+        // Make sure port is valid
         if (port < 1024 || port > 65535) {
-            return "Invalid port #.  Must be between 1024 and 65535 (inclusive)";
+            throw new IOException("Invalid port #.  Must be between 1024 and 65535 (inclusive)");
         }
+
+        // Start the server
+        ServerSocket server = new ServerSocket(port);
         
-        ServerSocket server;
+        // Listen for data on the wire
+        Socket listening = server.accept();
+
+        // Get the stream to write to our socket
+        OutputStream output = listening.getOutputStream();
+
+        // Listen for input
+        Scanner s = new Scanner(listening.getInputStream());
+
+        // Grab a message
+        String input = s.hasNext() ? s.next() : "";
+
+        // Display message
+        System.out.println(input+"\n");
+
+        DatagramSocket portForTransaction = null;
         
-        try {
-            
-            server = new ServerSocket(port);
-            System.out.println("TCP Negotiation Server started on port " + port + "\n");
-            
-            Socket listening = server.accept();
+        // If we recieve start code from our socket
+        if (input.equals("123")) {
 
-            // Get the stream to write to our socket
-            OutputStream output = listening.getOutputStream();
+            // Get a port currently available
+            portForTransaction = GetRandomSocketForTransaction();
 
-            // Listen for input
-            Scanner s = new Scanner(listening.getInputStream());
             
-            System.out.println("Waiting for data...");
-            
-            String input = s.hasNext() ? s.next() : "";
-            
-            System.out.println("Recieved over socket: " + input);
+            // Send the port we want to use for transaction
+            output.write(String.valueOf(portForTransaction.getLocalPort()).getBytes("UTF-8"));
 
-            // If we recieve something from our socket
-            if (input.equals("123")) {
+            System.out.println("Negotiation detected. Selected the following random port " + portForTransaction.getLocalPort() + "\n");
 
-                // Get a port currently available
-                int portForTransaction = GetFreePortForTransaction();
-
-                // Send the port we want to use for transaction
-                output.write(String.valueOf(portForTransaction).getBytes(Charset.forName("UTF-8")));
-                
-                transactionServerPort = portForTransaction;
-                
-                System.out.println("Negotiation detected. Selected the following random port " + portForTransaction);
-                
-            }
-            
-            listening.close();
-            
-        } catch (IOException e) {
-            return e.getMessage();
         }
-        
-        return "";
-        
+
+        listening.close();
+
+       return portForTransaction;
+
     }
-    
-    public static String StartTransferServer(int port) {
-        
-        DatagramSocket socket;
+
+    /**
+     * Takes a UDP socket and begins listening for file transfer
+     * 
+     * @param socket
+     * @return errors that occur in the process
+     */
+    public static String StartTransferServer(DatagramSocket socket) {
+
         DatagramPacket inPacket = new DatagramPacket(new byte[4], 4);
         DatagramPacket outPacket = new DatagramPacket(new byte[4], 4);
-
-        // Initialize the socket
+        PrintWriter writer;
+        
+        // Initialize file we're going to be writing too.
         try {
-            socket = new DatagramSocket(port);
-        } catch (SocketException ex) {
+            writer = new PrintWriter(new File("./output.txt"));
+        } catch (IOException ex) {
             return ex.getMessage();
         }
-        
-        System.out.println("UDP Transfer Server started on port " + port + "\n");
-        
+
+        // Stay in this state till someone sends an EOT character
         while (true) {
 
             // Grab a packet of data
@@ -137,7 +147,7 @@ public class Server {
             } catch (IOException ex) {
                 return ex.getMessage();
             }
-            
+
             String payload;
 
             // Grab payload
@@ -146,16 +156,17 @@ public class Server {
             } catch (UnsupportedEncodingException ex) {
                 return ex.getMessage();
             }
-            
-            System.out.print("Payload: " + payload);
 
+            // Write payload to output
+            writer.write(payload.split("\4")[0]);
+            
             // Create new payload
             try {
                 outPacket.setData(payload.toUpperCase().getBytes("US-ASCII"));
             } catch (UnsupportedEncodingException ex) {
                 return ex.getMessage();
             }
-            
+
             // Set routing for return to sender
             outPacket.setSocketAddress(inPacket.getSocketAddress());
 
@@ -170,14 +181,31 @@ public class Server {
             if (payload.contains("\4")) {
                 break;
             }
-            
+
         }
         
+        // Close everything up
+        socket.close();
+        writer.close();
+
         return null;
     }
-    
-    public static int GetFreePortForTransaction() {
-        return 3000;
+
+    /**
+     * Creates a socket between 1024 and 65535
+     * 
+     * @return a UDP Socket with a random port 
+     */
+    public static DatagramSocket GetRandomSocketForTransaction() {
+        // Look for a port until we find one
+        while(true){
+            try {
+                // Try creating a socket between 1024 and 65535
+                return new DatagramSocket(1024 + (int)(Math.random() * 64511));
+            } catch (SocketException ex) {
+                
+            }
+        }
     }
-    
+
 }
